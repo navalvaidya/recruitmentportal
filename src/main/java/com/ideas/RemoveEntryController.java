32089da14b63f06f661c183cb12b304653d02cd7
navalvaidya <naval.2402@gmail.com>
2015-07-03 18:07:27 +0530

    Remove Functionality Added

---
+++ src/main/java/com/ideas/RemoveEntryController.java
@@ -0,0 +1,62 @@
+package com.ideas;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RemoveEntryController
+ */
+public class RemoveEntryController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RemoveEntryController() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LoadConfigFile config=new LoadConfigFile();
+	    String DBUrl=config.DBUrl();
+	    String DBPasswd=config.DBPasswd();
+	    String DBUser=config.DBUser();
+		String id=request.getParameter("id");
+		System.out.println(id);
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Connection con = DriverManager.getConnection(DBUrl,DBUser,DBPasswd);
+			PreparedStatement stat2=con.prepareStatement("DELETE FROM employee WHERE id=?");
+			stat2.setString(1, id);
+			stat2.executeUpdate();
+			stat2.close();
+			con.close();
+			RequestDispatcher rd=request.getRequestDispatcher("/main.html");  
+	        rd.forward(request, response); 
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		
+	}
+
+}
